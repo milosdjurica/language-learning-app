dcdf6448c19397d38e15562281240d393e276da1
milosdjurica <milosdjurica13@gmail.com>
2023-10-17 00:49:42 +0200

    installed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import ThemeSwitchButton from "@/components/ThemeSwitchButton";
 import { getServerSession } from "next-auth/next";
 
 export default async function Home() {
   const session2 = await getServerSession();
 
-  // console.log(session2);
   return (
     <div>
       <h1 className="text-6xl">Start</h1>
       <p>{session2?.user?.name}</p>
+      <h2 className="bg-slate-400">Opa</h2>
+      <h2 className="bg-gray-400">Opa</h2>
+      <h2 className="bg-zinc-400">Opa</h2>
+      <h2 className="bg-neutral-400">Opa</h2>
+      <h2 className="bg-stone-400">Opa</h2>
     </div>
   );
 }
-``;
