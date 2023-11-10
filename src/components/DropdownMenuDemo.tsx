1563a2634971274fa70a153bdae4e81f3cb3b152
milosdjurica <milosdjurica13@gmail.com>
2023-11-10 16:02:24 +0100

    removed links from navbar until i create other pages

--- src/components/DropdownMenuDemo.tsx
+++ src/components/DropdownMenuDemo.tsx
@@ -1,19 +1,4 @@
-import {
-  Cloud,
-  CreditCard,
-  Github,
-  Keyboard,
-  LifeBuoy,
-  LogOut,
-  Mail,
-  MessageSquare,
-  Plus,
-  PlusCircle,
-  Settings,
-  User,
-  UserPlus,
-  Users,
-} from "lucide-react";
+import { CreditCard, LogOut, Settings, User } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -48,25 +33,19 @@ export function DropdownMenuDemo() {
 
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          {/* <DropdownMenuItem> */}
           <DropdownMenuItem className="flex justify-between">
             <User className="mr-2 h-4 w-4" />
             <span>Profile</span>
-            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
           </DropdownMenuItem>
 
-          {/* <DropdownMenuItem> */}
           <DropdownMenuItem className="flex justify-between">
             <CreditCard className="mr-2 h-4 w-4" />
             <span>Billing</span>
-            {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
           </DropdownMenuItem>
 
-          {/* <DropdownMenuItem> */}
           <DropdownMenuItem className="flex justify-between">
             <Settings className="mr-2 h-4 w-4" />
             <span>Settings</span>
-            {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
           </DropdownMenuItem>
         </DropdownMenuGroup>
 
@@ -74,7 +53,6 @@ export function DropdownMenuDemo() {
         <DropdownMenuItem onClick={() => signOut()}>
           <LogOut className="mr-2 h-4 w-4" />
           <span>Log out</span>
-          {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import React from "react";
-import { NavBar } from "./NavBar";
-import { DropdownMenuDemo } from "./DropdownMenuDemo";
+import { ThemeSwitchButton } from "./ThemeSwitchButton";
+import { signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className="flex justify-between px-4 py-2 items-center border-b border-border">
-      <NavBar />
-      <DropdownMenuDemo />
+      <ThemeSwitchButton />
+      <h3>
+        Welcome back &#34;{session?.user?.name || session?.user?.email}&#34;
+      </h3>
+      <Button
+        variant="outline"
+        className="border border-primary"
+        onClick={() => signOut()}
+      >
+        <LogOut className="mr-2 h-4 w-4" />
+        <span>Log out</span>
+      </Button>
+
+      {/* // TODO change to this when add other functionalities */}
+      {/* <NavBar /> */}
+      {/* <DropdownMenuDemo /> */}
     </header>
   );
 }
